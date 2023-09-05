7de3a57b2c14af9238cc3b5a4c845f74ab26fccb
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-09-06 01:31:20 +0530

    first commit

---
+++ Assisgnment18/Data/Assisgnment18DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assisgnment18.Models;
+
+namespace Assisgnment18.Data
+{
+    public class Assisgnment18DbContext : DbContext
+    {
+        public Assisgnment18DbContext (DbContextOptions<Assisgnment18DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assisgnment18.Models.Movie> Movie { get; set; } = default!;
+    }
+}
